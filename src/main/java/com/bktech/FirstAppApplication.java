923458477b4b5f01387a64987c2f38208a6035db
Bunty <Bunty@HP>
2022-08-02 15:34:32 +0530

    First File Added

---
+++ src/main/java/com/bktech/FirstAppApplication.java
@@ -0,0 +1,15 @@
+package com.bktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class FirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppApplication.class, args);
+	}
+
+}
